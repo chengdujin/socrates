0c1d14583406d3b3a96c4c029a83d550b81a2e25
chengdujin <chengdujin@gmail.com>
2012-03-10 23:42:53 +0800

    the initialization and read from mongodb methods are added to the interest_generator.py

--- generator/interest_generator.py
+++ generator/interest_generator.py
@@ -20,10 +20,85 @@ sys.setdefaultencoding('UTF-8')
 SOURCE = 'twitter/perryhau.chinese'
 DB = '176.34.54.120:27017'
 
+doc_top = []
+top_voc = []
+number_topic = 7
+alpha = 2 
+beta = .5
+iteration = 600 
+burnin = 300
 
-def initialize(segs):
+
+def initialize(docs, vocabs):
+    'initialize doc_vocab, vocab_topic, doc_topic and topic lists'
+    import random
+    index_vocab = vocabs[0]
+    vocab_index = vocabs[1]
+    number_of_vocab = len(index_vocab)
+    doc_vocab = {}
+    vocab_topic = {}
+    doc_topic = {}
+    topic = {}
+    for doc_id, doc in enumerate(docs):
+        dc = {} 
+	for word in doc:
+	    rand_topic_index_vocab_id = random.randint(1, number_of_vocab) - 1
+            word_vocab_id = vocab_index[word]
+	    # build doc-vocab
+            dc[word_vocab_id] = rand_topic_index_vocab_id
+
+    	    # build vocab-topic
+	    if not word_vocab_id in vocab_topic:
+	        vt = {}
+		vt[rand_topic_index_vocab_id] = 1
+	    else:
+		vt = vocab_topic[word_vocab_id]
+		if rand_topic_index_vocab_id in vt:
+	            vt[rand_topic_index_vocab_id] += 1
+		else:
+		    vt[rand_topic_index_vocab_id] = 1
+            vocab_topic[word_vocab_id] = vt
+
+	    # build doc-topic
+	    if not doc_id in doc_topic:
+		dt = {}
+		dt[rand_topic_index_vocab_id] = 1
+	    else:
+		if rand_topic_index_vocab_id in dt:
+		    print doc_id, rand_topic_index_vocab_id
+		    dt[rand_topic_index_vocab_id] += 1
+    		else:
+		    dt[rand_topic_index_vocab_id] = 1
+	    doc_topic[doc_id] = dt
+            
+	    # build topic
+	    if not rand_topic_index_vocab_id in topic:
+		topic[rand_topic_index_vocab_id] = 1
+	    else:
+		topic[rand_topic_index_vocab_id] += 1
+        doc_vocab[doc_id] = dc 
+    
+    return (doc_vocab, vocab_topic, doc_topic, topic)
+
+def vocab_indexer(docs):
+    'create an word_id-word list, and word-word_id dict'
+    # word_id --> word
+    index_vocab = []
+    # word --> word_id
+    vocab_index = {}
+
+    for doc in docs:
+        for word in doc:
+	    if word not in vocab_index:     
+		index = len(index_vocab)
+		index_vocab.append(word)
+		vocab_index[word] = index
+    return (index_vocab, vocab_index)
+
+def prepare(docs):
     ''
-    pass
+    vocabs = vocab_indexer(docs)
+    doc_vocab_topic = initialize(docs, vocabs)    
 
 def read(source):
     'simply read segments from mongodb'
@@ -46,7 +121,7 @@ def read(source):
 		docs.append(entry['seg'])
 	return docs
     else:
-	return Exception("[errot] read: nothing is found!")      
+	return Exception("[error] read: nothing is found!")      
 
 def generate(source):
     '''
@@ -57,10 +132,10 @@ def generate(source):
     5. publish the result
     '''
     seg_list = read(source)
-    initialize(seg_list)
-    screen()
-    learn()
-    publish()
+    prepare(seg_list)
+    #screen()
+    #learn()
+    #publish()
 
 if __name__ == '__main__':
     generate(SOURCE)
