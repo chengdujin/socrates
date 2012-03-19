5ded5ab73b70f96e872289cd2316cc154b662a6f
chengdujin <chengdujin@gmail.com>
2012-03-19 22:15:15 +0800

    move lda implementation from interest_generator.py to lda.py

--- generator/interest_generator.py
+++ generator/interest_generator.py
@@ -8,7 +8,7 @@
 # @author Yuan JIN
 # @contact chengdujin@gmail.com
 # @since 2012.03.10
-# @latest 2012.03.10
+# @latest 2012.03.19
 #
 
 import random
@@ -21,201 +21,12 @@ sys.setdefaultencoding('UTF-8')
 SOURCE = 'twitter/chengdujin.chinese'
 DB = '176.34.54.120:27017'
 
-TOPIC_NUMBER = 20
-ALPHA = 2 
-BETA = .5
-ITERATION = 600 
-burnin = 300
-
-
-def publish(docs, vocabs, data):
-    'simple publication'
-    from collections import OrderedDict
-
-    index_vocab = vocabs[0]
-    vocab_index = vocabs[1]
-
-    doc_vocab = data[0]
-    vocab_topic = data[1]
-    doc_topic = data[2]
-    topics = data[3]
-    documents = data[4]
-
-    # build topic-vocab map
-    topic_vocab = {}
-    for word_vocab_id in vocab_topic:
-	    vt = vocab_topic[word_vocab_id]
-        sorted_vt = OrderedDict(sorted(vt.items(), key=lambda x: -x[1]))
-        for svt, (k, v) in enumerate(sorted_vt.items()):
-	        if svt > 3:
-		        break
-	        else:
-                if not k in topic_vocab:
-		            vocab_list = {}
-		            vocab_list[word_vocab_id] = v
-		        else:
-		            vocab_list = topic_vocab[k]
-		    if word_vocab_id in vocab_list:
-			    old_association = vocab_list[word_vocab_id]
-			    if old_association < v:
-			        vocab_list[word_vocab_id] = v
-		        else:
-			        vocab_list[word_vocab_id] = v
-		    topic_vocab[k] = vocab_list
-
-    # combine topic-vocab with index_vocab to create a topic_id --> vocabulary list map
-    topic_word = {}
-    for topic_id in topic_vocab:
-	    topic_word[topic_id] = ''
-	    tv = topic_vocab[topic_id]
-	    sorted_tv = OrderedDict(sorted(tv.items(), key=lambda x: -x[1]))
-	    for stv, (k, v) in enumerate(sorted_tv.items()):
-	        if stv > 3:
-		        break
-	        else:
-		        topic_word[topic_id] += index_vocab[k] + ',' 
-
-    # publish
-    for doc_id, doc in enumerate(docs):
-	    if doc_id in doc_topic and len(doc) > 2:
-	        topic_list = doc_topic[doc_id]
-            sorted_topic_list =  OrderedDict(sorted(topic_list.items(), key=lambda x: -x[1]))
-	        wanted_strings = []
-            for stl, (k, v) in enumerate(sorted_topic_list.items()):
-                if stl > 3:
-		            break
-	            else:
-	                wanted_strings.append(topic_word[k])
-	    print str(doc_id + 1) + '.' +  ','.join(doc)
-	    print '>> ' + ' '.join(wanted_strings)
-	    print
-
-def learn(vocabs, data):
-    'lda implementation'
-    index_vocab = vocabs[0]
-    vocab_index = vocabs[1]
-
-    doc_vocab = data[0]
-    vocab_topic = data[1]
-    doc_topic = data[2]
-    topics = data[3]
-    documents = data[4]
-    for it in xrange(0, ITERATION):
-	for doc_id in doc_vocab:
-	    for word_vocab_id in doc_vocab[doc_id]:
-		# sample full conditional
-	        topic = doc_vocab[doc_id][word_vocab_id]
-	        vocab_topic[word_vocab_id][topic] -= 1
-		doc_topic[doc_id][topic] -= 1
-		topics[topic] -= 1		
-		documents[doc_id] -= 1
-
-		# multinomial sampling via cumulative method
-		probability = {}
-	        for k in xrange(0, TOPIC_NUMBER):
-		    probability[k] = (vocab_topic[word_vocab_id][topic] + BETA) / (topics[topic] + len(vocab_index) * BETA) * (doc_topic[doc_id][topic] + ALPHA) / (documents[doc_id] + TOPIC_NUMBER * ALPHA)			
-		# cumulate multinomial parameters
-		for k in xrange(1, len(probability)):
-		    probability[k] += probability[k - 1];
-
-		# caled sample because of unnormalised probability[]
-	        u = random.random() * probability[TOPIC_NUMBER -1]
-		for topic in xrange(0, len(probability)):
-		    if probability[topic] > u:
-			break	
-
-		# add back new estimated value
-		if not topic in vocab_topic[word_vocab_id]:
-		    vocab_topic[word_vocab_id][topic] = 1
-		else:
-		    vocab_topic[word_vocab_id][topic] += 1
-		if not topic in doc_topic[doc_id]:
-                    doc_topic[doc_id][topic] = 1
-		else:
-		    doc_topic[doc_id][topic] += 1
-		if not topic in topics:
-		    topics[topic] = 1
-		else:
-                    topics[topic] += 1        
-                documents[doc_id] += 1	
-		doc_vocab[doc_id][word_vocab_id] = topic
-
-    return (doc_vocab, vocab_topic, doc_topic, topics, documents)
-
-def initialize(docs, vocabs):
-    'initialize doc_vocab, vocab_topic, doc_topic and topic lists'
-    index_vocab = vocabs[0]
-    vocab_index = vocabs[1]
-    doc_vocab = {}
-    vocab_topic = {}
-    doc_topic = {}
-    topics = {}
-    documents = {}
-    for doc_id, doc in enumerate(docs):
-        dc = {} 
-        # build documents
-	    documents[doc_id] = len(doc)
-	    for word in doc:
-	        rand_topic = random.randint(1, TOPIC_NUMBER)
-            word_vocab_id = vocab_index[word]
-	        # build doc-vocab
-            dc[word_vocab_id] = rand_topic
-
-    	    # build vocab-topic
-	        if not word_vocab_id in vocab_topic:
-	            vt = {}
-		        vt[rand_topic] = 1
-	        else:
-		        vt = vocab_topic[word_vocab_id]
-		    if rand_topic in vt:
-	            vt[rand_topic] += 1
-		    else:
-		        vt[rand_topic] = 1
-                vocab_topic[word_vocab_id] = vt
-
-	    # build doc-topic
-	    if not doc_id in doc_topic:
-		    dt = {}
-		    dt[rand_topic] = 1
-	    else:
-		    if rand_topic in dt:
-		        dt[rand_topic] += 1
-    		else:
-		        dt[rand_topic] = 1
-	    doc_topic[doc_id] = dt
-            
-	    # build topic
-	    if not rand_topic in topics:
-		    topics[rand_topic] = 1
-	    else:
-		    topics[rand_topic] += 1
-        doc_vocab[doc_id] = dc 
-    
-    return (doc_vocab, vocab_topic, doc_topic, topics, documents)
-
-def vocab_indexer(docs):
-    'create an word_id-word list, and word-word_id dict'
-    # word_id --> word
-    index_vocab = []
-    # word --> word_id
-    vocab_index = {}
-
-    for doc in docs:
-        for word in doc:
-	        if word not in vocab_index:     
-		        index = len(index_vocab)
-		        index_vocab.append(word)
-		        vocab_index[word] = index
-    return (index_vocab, vocab_index)
-
-def prepare(docs):
-    'create data structure and initialize them'
-    vocabs = vocab_indexer(docs)
-    doc_vocab_topic = initialize(docs, vocabs)    
-    return vocabs, doc_vocab_topic
 
 def read(source):
     'simply read segments from mongodb'
+    sys.path.append('../extractor')
+    from media import Segment
+
     lines = source.split('/')
     head = lines[0]
     content = lines[1]
@@ -255,10 +66,11 @@ def generate(source):
     5. publish the result
     '''
     seg_list = read(source)
-    vocabs, doc_vocab_topic = prepare(seg_list)
+    import lda
+    topic_extractor = lda.LDA(seg_list)
     #screen()
-    data = learn(vocabs, doc_vocab_topic)
-    publish(seg_list, vocabs, data)
+    topic_extractor.learn()
+    topic_extractor.publish()
 
 if __name__ == '__main__':
     generate(SOURCE)
--- generator/lda.py
+++ generator/lda.py
@@ -27,161 +27,146 @@ class LDA(object):
     'class to model lda computation'
     def __init__(self, docs):
         'create data structure and initialize them'
-        self.index_vocab, self.vocab_index = self.vocab_indexer(docs)
-        self.doc_vocab_topic = self.initialize(docs, vocabs)    
+        self.docs = docs
+        self.index_vocab, self.vocab_index = self.indexer()
+        self.doc_vocab, self.vocab_topic, self.doc_topic, self.topics, self.documents = self.initialize()    
 
-    def vocab_indexer(docs):
+    def indexer(self):
         'create an word_id-word list, and word-word_id dict'
         # word_id --> word
         index_vocab = []
         # word --> word_id
         vocab_index = {}
 
-        for doc in docs:
-            for word in doc:
-                if word not in vocab_index:
-                    index = len(index_vocab)
-                    index_vocab.append(word)
-                    vocab_index[word] = index
-        return (index_vocab, vocab_index)
+        # a 'terms' refers to all the segmented words of a tweet/weibo
+        for terms in self.docs:
+            # a term is an instance of media.Segment class
+            for term in terms:
+                if term not in vocab_index:
+                    index_number = len(index_vocab)
+                    index_vocab.append(term)
+                    vocab_index[term] = index_number
+        return index_vocab, vocab_index
 
-    def initialize(docs, vocabs):
+    def initialize(self):
         'initialize doc_vocab, vocab_topic, doc_topic and topic lists'
-        index_vocab = vocabs[0]
-        vocab_index = vocabs[1]
         doc_vocab = {}
         vocab_topic = {}
         doc_topic = {}
         topics = {}
         documents = {}
-        for doc_id, doc in enumerate(docs):
-            dc = {}
+
+        for doc_id, doc in enumerate(self.docs):
             # build documents
             documents[doc_id] = len(doc)
+
+            dc = {}
+            # a word is an instance of Segment
             for word in doc:
                 rand_topic = random.randint(1, TOPIC_NUMBER)
-                word_vocab_id = vocab_index[word]
+                word_vocab_id = self.vocab_index[word]
                 # build doc-vocab
                 dc[word_vocab_id] = rand_topic
 
                 # build vocab-topic
-                if not word_vocab_id in vocab_topic:
+                if word_vocab_id not in vocab_topic:
                     vt = {}
                     vt[rand_topic] = 1
                 else:
                     vt = vocab_topic[word_vocab_id]
-                if rand_topic in vt:
-                    vt[rand_topic] += 1
-                else:
-                    vt[rand_topic] = 1
-                    vocab_topic[word_vocab_id] = vt
+                    if rand_topic in vt:
+                        vt[rand_topic] += 1
+                    else:
+                        vt[rand_topic] = 1
+                vocab_topic[word_vocab_id] = vt
 
-            # build doc-topic
-            if not doc_id in doc_topic:
-                dt = {}
-                dt[rand_topic] = 1
-            else:
-                if rand_topic in dt:
-                    dt[rand_topic] += 1
-                else:
+                # build doc-topic
+                if doc_id not in doc_topic:
+                    dt = {}
                     dt[rand_topic] = 1
-            doc_topic[doc_id] = dt
+                else:
+                    if rand_topic in dt:
+                        dt[rand_topic] += 1
+                    else:
+                        dt[rand_topic] = 1
+                doc_topic[doc_id] = dt
 
-            # build topic
-            if not rand_topic in topics:
-                topics[rand_topic] = 1
-            else:
-                topics[rand_topic] += 1
+                # build topic
+                if rand_topic not in topics:
+                    topics[rand_topic] = 1
+                else:
+                    topics[rand_topic] += 1
             doc_vocab[doc_id] = dc
+        return doc_vocab, vocab_topic, doc_topic, topics, documents
 
-        return (doc_vocab, vocab_topic, doc_topic, topics, documents)
-
-    def learn(vocabs, data):
+    def learn(self):
         'lda implementation'
-        index_vocab = vocabs[0]
-        vocab_index = vocabs[1]
-
-        doc_vocab = data[0]
-        vocab_topic = data[1]
-        doc_topic = data[2]
-        topics = data[3]
-        documents = data[4]
         for it in xrange(0, ITERATION):
-        for doc_id in doc_vocab:
-            for word_vocab_id in doc_vocab[doc_id]:
-            # sample full conditional
-                topic = doc_vocab[doc_id][word_vocab_id]
-                vocab_topic[word_vocab_id][topic] -= 1
-            doc_topic[doc_id][topic] -= 1
-            topics[topic] -= 1
-            documents[doc_id] -= 1
+            for doc_id in self.doc_vocab:
+                for word_vocab_id in self.doc_vocab[doc_id]:
+                    # sample full conditional
+                    topic = self.doc_vocab[doc_id][word_vocab_id]
+                    self.vocab_topic[word_vocab_id][topic] -= 1
+                    self.doc_topic[doc_id][topic] -= 1
+                    self.topics[topic] -= 1
+                    self.documents[doc_id] -= 1
 
-            # multinomial sampling via cumulative method
-            probability = {}
+                # multinomial sampling via cumulative method
+                probability = {}
                 for k in xrange(0, TOPIC_NUMBER):
-                probability[k] = (vocab_topic[word_vocab_id][topic] + BETA) / (topics[topic] + len(vocab_index) * BETA) * (doc_topic[doc_id][topic] + ALPHA) / (documents[doc_id] + TOPIC_NUMBER * ALPHA)
-            # cumulate multinomial parameters
-            for k in xrange(1, len(probability)):
-                probability[k] += probability[k - 1];
+                    probability[k] = (self.vocab_topic[word_vocab_id][topic] + BETA) / (self.topics[topic] + len(self.vocab_index) * BETA) * (self.doc_topic[doc_id][topic] + ALPHA) / (self.documents[doc_id] + TOPIC_NUMBER * ALPHA)
+                
+                # cumulate multinomial parameters
+                for k in xrange(1, len(probability)):
+                    probability[k] += probability[k - 1];
 
-            # caled sample because of unnormalised probability[]
+                # scaled sample because of unnormalised probability[]
                 u = random.random() * probability[TOPIC_NUMBER -1]
-            for topic in xrange(0, len(probability)):
-                if probability[topic] > u:
-                break
+                for topic in xrange(0, len(probability)):
+                    if probability[topic] > u:
+                        break
 
-            # add back new estimated value
-            if not topic in vocab_topic[word_vocab_id]:
-                vocab_topic[word_vocab_id][topic] = 1
-            else:
-                vocab_topic[word_vocab_id][topic] += 1
-            if not topic in doc_topic[doc_id]:
-                    doc_topic[doc_id][topic] = 1
-            else:
-                doc_topic[doc_id][topic] += 1
-            if not topic in topics:
-                topics[topic] = 1
-            else:
-                    topics[topic] += 1
-                documents[doc_id] += 1
-            doc_vocab[doc_id][word_vocab_id] = topic
+                # add back new estimated value
+                if topic not in self.vocab_topic[word_vocab_id]:
+                    self.vocab_topic[word_vocab_id][topic] = 1
+                else:
+                    self.vocab_topic[word_vocab_id][topic] += 1
+                if not topic in self.doc_topic[doc_id]:
+                    self.doc_topic[doc_id][topic] = 1
+                else:
+                    self.doc_topic[doc_id][topic] += 1
+                if topic not in topics:
+                    self.topics[topic] = 1
+                else:
+                    self.topics[topic] += 1
+                self.documents[doc_id] += 1
+                self.doc_vocab[doc_id][word_vocab_id] = topic
 
-        return (doc_vocab, vocab_topic, doc_topic, topics, documents)
-
-    def publish(docs, vocabs, data):
+    def publish(self):
         'simple publication'
         from collections import OrderedDict
 
-        index_vocab = vocabs[0]
-        vocab_index = vocabs[1]
-
-        doc_vocab = data[0]
-        vocab_topic = data[1]
-        doc_topic = data[2]
-        topics = data[3]
-        documents = data[4]
-
         # build topic-vocab map
         topic_vocab = {}
-        for word_vocab_id in vocab_topic:
-            vt = vocab_topic[word_vocab_id]
+        for word_vocab_id in self.vocab_topic:
+            vt = self.vocab_topic[word_vocab_id]
             sorted_vt = OrderedDict(sorted(vt.items(), key=lambda x: -x[1]))
             for svt, (k, v) in enumerate(sorted_vt.items()):
                 if svt > 3:
                     break
                 else:
-                    if not k in topic_vocab:
+                    if k not in topic_vocab:
                         vocab_list = {}
                         vocab_list[word_vocab_id] = v
                     else:
                         vocab_list = topic_vocab[k]
-                if word_vocab_id in vocab_list:
-                    old_association = vocab_list[word_vocab_id]
-                    if old_association < v:
-                        vocab_list[word_vocab_id] = v
-                    else:
-                        vocab_list[word_vocab_id] = v
-                topic_vocab[k] = vocab_list
+                        if word_vocab_id in vocab_list:
+                            old_association = vocab_list[word_vocab_id]
+                            if old_association < v:
+                                vocab_list[word_vocab_id] = v
+                            else:
+                                vocab_list[word_vocab_id] = v
+                    topic_vocab[k] = vocab_list
 
         # combine topic-vocab with index_vocab to create a topic_id --> vocabulary list map
         topic_word = {}
@@ -196,9 +181,9 @@ class LDA(object):
                     topic_word[topic_id] += index_vocab[k] + ','
 
         # publish
-        for doc_id, doc in enumerate(docs):
-            if doc_id in doc_topic and len(doc) > 2:
-                topic_list = doc_topic[doc_id]
+        for doc_id, doc in enumerate(self.docs):
+            if doc_id in self.doc_topic and len(doc) > 2:
+                topic_list = self.doc_topic[doc_id]
                 sorted_topic_list =  OrderedDict(sorted(topic_list.items(), key=lambda x: -x[1]))
                 wanted_strings = []
                 for stl, (k, v) in enumerate(sorted_topic_list.items()):
