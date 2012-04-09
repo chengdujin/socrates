c854baa88fca79be96a20084d692e993f88a51e1
EC2 Default User <chengdujin@gmail.com>
2012-04-09 10:24:45 +0000

    added a simple templates directory and a sample file

--- exposer/rest/flask-server.py
+++ exposer/rest/flask-server.py
@@ -1,15 +1,17 @@
-from flask import Flask                                                                                                                                                  
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world!'
+    return render_template('hello.html')
 
 @app.route('/people/<person>')
 def test(person):
     if person:
-        return 'Welcom, %s!' % person
+        return 'bienvenue, %s!' % person
     return type(person)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
